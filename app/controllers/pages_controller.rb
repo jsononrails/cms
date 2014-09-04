08d53e196049f1f80e63c9054d1e1395f7dfef4a
Jason McBride <jsononrails@gmail.com>
2014-09-04 19:48:06 -0400

    fixed 404, removed user register link

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -22,7 +22,11 @@ class PagesController < ApplicationController
       @home ||= Page.find_by_slug('home')
       @home_children ||= @home.children.order("position ASC")
       @page ||= Page.find_by_slug(params[:id])
-
+      
+      if !@page
+        @page = Page.find_by_slug("error-404")
+      end
+      
       @page_children ||= @page.children.order("position ASC")
       @layout ||= @page.layout ? "layouts/#{@page.layout.layout}".downcase : "layouts/application"
       @footer_about ||= Page.where("name = ? AND is_published = ?", "About Sault Sports Medicine", true).take
